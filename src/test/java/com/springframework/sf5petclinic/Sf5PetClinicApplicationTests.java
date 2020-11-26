4ae69e47e279e5586c0540c185b0953def20ee03
amin rezazade <amin_r359@yahoo.com>
2020-11-26 16:47:33 +0330

    initiate spring project

---
+++ src/test/java/com/springframework/sf5petclinic/Sf5PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sf5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf5PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
