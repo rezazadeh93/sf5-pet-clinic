6ba171750cbd261e84cc1873e59e951628d596ba
Amin <amin.rezazadeh93@gmail.com>
2020-12-20 17:03:30 +0330

    added visit entity jpa

--- pet-clinic-data/src/main/java/com/springframework/sf5petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/sf5petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.springframework.sf5petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,18 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
